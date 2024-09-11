c13b859b281b93fd57c2af0c0065656b72655d73
ana luisa miranda <analuisamirandap@gmail.com>
2024-09-11 15:59:04 -0300

    distributing code in components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { TaskList } from "./components/TaskList";
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {id: 1, name: "Record Lectures", completed: false}, 
-    {id: 2, name: "Take shower", completed: true}, 
-    {id: 3, name: "Cook Dinner", completed: false}
-  ]);
 
-  const [show, setShow] = useState(true);
-
-  function handleDelete(id){
-    setTasks(tasks.filter(task => id !== task.id))
-  }
-
-    return (
-      <div className='App'>
-        <h1>TaskList</h1>
-        <ul>
-          <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
-          { show && tasks.map((task) => (
-            <li key={task.id} className={task.completed ? "completed" : "incompleted"}>
-              <span>{task.id} - {task.name}</span>
-              <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <TaskList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
